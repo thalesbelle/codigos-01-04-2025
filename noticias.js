609ab6a0e662a346a760caf277e88709636719d7
Thales <thalesbelle0@gmail.com>
2025-04-10 21:08:45 -0300

    site atualizado

--- noticias.js
+++ noticias.js
@@ -3,12 +3,15 @@ const titulonoticia1 = document.querySelector("#titulonoticia1");
 const conteudonoticia1 = document.querySelector("#conteudonoticia1");
 const kanye = document.querySelector("#kanye");
 const imagemKanye = document.querySelector("#imagemKanye");
+const iconeMusica = document.querySelector("#iconeMusica");
 
 noticia1.addEventListener("click", function () {
     titulonoticia1.toggleAttribute("hidden");
     conteudonoticia1.toggleAttribute("hidden");
     imagemKanye.classList.toggle("noticiaHover1");
     kanye.classList.toggle("noticiaHover2");
+    noticia1.classList.toggle("noticia1Expansao");
+    iconeMusica.toggleAttribute("hidden");
 });
 
 imagemKanye.addEventListener("click", function () {
@@ -16,4 +19,6 @@ imagemKanye.addEventListener("click", function () {
     conteudonoticia1.toggleAttribute("hidden");
     imagemKanye.classList.toggle("noticiaHover1");
     kanye.classList.toggle("noticiaHover2");
+    noticia1.classList.toggle("noticia1Expansao");
+    iconeMusica.toggleAttribute("hidden");
 });
